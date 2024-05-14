a471e56f0b4c305f23b8f29393d55ed0a2e13b76
larinhab <larabviana@outlook.com>
2024-05-14 09:56:20 -0300

    :new: first commit - initial configs

---
+++ src/styles/global.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color:  ${({ theme }) => theme.COLORS.DARK_BLUE_900};;
+        color:  ${({ theme }) => theme.COLORS.LIGHT_300};
+
+        -webkit-font-smoothing: antialiased;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button {
+        cursor: pointer;
+        color:  ${({ theme }) => theme.COLORS.TOMATO_100};
+    }
+
+    `
---
+++ src/styles/theme.js
@@ -0,0 +1,86 @@
+// CORES GLOBAIS
+
+export default {
+    COLORS: {
+      DARK_BLUE_200: "#00070A",
+      DARK_BLUE_300: "#000204",
+      DARK_BLUE_400: "#000A0F",
+      DARK_BLUE_700: "#001119",
+      DARK_BLUE_600: "#00111A",
+      DARK_BLUE_900: "#0D1D25",
+
+      LIGHT_BLUE_100: "#82F3FF",
+      LIGHT_BLUE: "#065E7C",
+      TOMATO_100: "#750310",
+      
+      WHITE: "#F4EDE8",
+      
+      LIGHT_100: "#FFFF",
+      LIGHT_200: "#FFFAF1",
+      LIGHT_300: "#E1E1E6",
+      LIGHT_400: "#C4C4CC",
+
+      GRAY_500: "#7C7C8A",
+      GRAY_700: "#4D585E",
+    }
+  }
+
+  // ICONES 
+  // SEARCH
+  import { IoIosSearch } from "react-icons/io";
+  <IoIosSearch />
+
+  // USER 
+  import { FiUser } from "react-icons/fi";
+  <FiUser />
+  // EDIT USER
+  import { FaUserEdit } from "react-icons/fa";
+  <FaUserEdit />
+  // PASSWORD
+  import { RiLockPasswordLine } from "react-icons/ri";
+  <RiLockPasswordLine />
+  // EMAIL
+  import { MdOutlineEmail } from "react-icons/md";
+  <MdOutlineEmail />
+
+  // MAIS
+  import { LuPlus } from "react-icons/lu";
+  <LuPlus />
+
+  // MENOS
+  import { LuMinus } from "react-icons/lu";
+  <LuMinus />
+
+  // LOG OUT
+  import { RxExit } from "react-icons/rx";
+  <RxExit />
+
+  // HEART
+  import { LuHeart } from "react-icons/lu";
+  <LuHeart />
+
+  // left right
+  import { MdKeyboardArrowLeft } from "react-icons/md";
+  <MdKeyboardArrowLeft />
+  import { MdKeyboardArrowRight } from "react-icons/md";
+  <MdKeyboardArrowRight />
+
+  // CARRINHO
+  import { FaCartPlus } from "react-icons/fa6";
+  <FaCartPlus />
+
+  //  EDIT
+  import { LuClipboardEdit } from "react-icons/lu";
+  <LuClipboardEdit />
+
+  // CARTÃO
+  import { FaRegCreditCard } from "react-icons/fa6";
+  <FaRegCreditCard />
+
+  // PIX
+  import { FaPix } from "react-icons/fa6";
+  <FaPix />
+
+  // UPLOAD
+  import { IoCloudUploadOutline } from "react-icons/io5";
+  <IoCloudUploadOutline />
