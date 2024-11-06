d390f7c10f1180a96b7b511bee0e44bc939ef760
Lara <larabviana@outlook.com>
2024-11-06 14:48:41 -0300

    :sparkles: validate login

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,86 +1,128 @@
-import React, { createContext, useState, useContext, useEffect } from 'react'
-import { api } from '../services/api'
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+} from "react";
+import { api } from "../services/api";
 
-export const AuthContext = createContext({})
+export const AuthContext =
+  createContext({});
 
-function AuthProvider({ children }){
-    const [ data, setData ] = useState({})
+function AuthProvider({ children }) {
+  const [data, setData] = useState({});
 
-    async function signIn({ email, password }){
-        try{
-            const response = await api.post("/sessions", { email, password })   
-            const { user, token } = response.data
+  async function signIn({
+    email,
+    password,
+  }) {
+    try {
+      const response = await api.post(
+        "/sessions",
+        { email, password },
+        { withCredentials: true }
+      );
+      const { user } = response.data;
 
-            localStorage.setItem("@foodexplorer:user", JSON.stringify(user))
-            localStorage.setItem("@foodexplorer:token", token)
+      localStorage.setItem(
+        "@foodexplorer:user",
+        JSON.stringify(user)
+      );
+      //  localStorage.setItem("@foodexplorer:token", token)
 
-            api.defaults.headers.common["Authorization"] = `Bearer ${token}`
-            setData({ user, token })
-
-        }catch(error){
-            if(error.response){
-                alert(error.response.data.message)
-            }else{
-                alert("Não foi possível entrar")
-            }
-        }
+      // api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+      setData({ user });
+    } catch (error) {
+      if (error.response) {
+        alert(
+          error.response.data.message
+        );
+      } else {
+        alert(
+          "Não foi possível entrar"
+        );
+      }
     }
+  }
 
-    function signOut(){
-        localStorage.removeItem("@foodexplorer:token")
-        localStorage.removeItem("@foodexplorer:user")
+  function signOut() {
+    // localStorage.removeItem(
+    //   "@foodexplorer:token"
+    // );
+    localStorage.removeItem(
+      "@foodexplorer:user"
+    );
 
-        setData({})
+    setData({});
+  }
+
+  async function updateProfile({
+    user,
+  }) {
+    try {
+      const response = await api.put(
+        `/users/${user.id}`,
+        user
+      );
+      const updatedUser = response.data;
+
+      localStorage.setItem(
+        "@foodexplorer:user",
+        JSON.stringify(updatedUser)
+      );
+      setData({
+        user: updatedUser,
+        token: data.token,
+      });
+
+      return "Usuário atualizado com sucesso!";
+    } catch (error) {
+      console.log(
+        "Não foi possivel atualizar o usuário.",
+        error
+      );
     }
+  }
 
-    async function updateProfile({ user }){
-        try {
-            const response = await api.put(`/users/${user.id}`, user)
-            const updatedUser = response.data
+  useEffect(() => {
+    // const token = localStorage.getItem(
+    //   "@foodexplorer:token"
+    // );
+    const user = localStorage.getItem(
+      "@foodexplorer:user"
+    );
 
-            localStorage.setItem("@foodexplorer:user", JSON.stringify((updatedUser)))
-            setData({ user: updatedUser, token: data.token})
-
-            return("Usuário atualizado com sucesso!")
-            
-        } catch (error) {
-            console.log("Não foi possivel atualizar o usuário.", error)
-        }
-        
+    if (user) {
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${token}`;
+      setData({
+        //token,
+        user: JSON.parse(user),
+      });
     }
+  }, []);
 
-
-    useEffect(() => {
-        const token = localStorage.getItem("@foodexplorer:token")
-        const user = localStorage.getItem("@foodexplorer:user")
-    
-        if(token && user){
-            api.defaults.headers.common["Authorization"] = `Bearer ${token}`
-                setData({ 
-                    token, 
-                    user: JSON.parse(user)
-                })
-        }    
-    }, [])
-    
-
-    return (
-        <AuthContext.Provider value={{ 
-            signIn, 
-            signOut, 
-            updateProfile,
-            user: data?.user 
-            }}>
-            
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        signOut,
+        updateProfile,
+        user: data?.user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-function useAuth(){
-    const context = useContext(AuthContext) 
+function useAuth() {
+  const context = useContext(
+    AuthContext
+  );
 
-    return context;
+  return context;
 }
 
 export { AuthProvider, useAuth };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,31 +1,47 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { AuthRoutes } from './auth.routes'
+import { AuthRoutes } from "./auth.routes";
 import { UserRoutes } from "./user.routes";
 import { AdminRoutes } from "./admin.routes";
 
 import { useAuth } from "../hooks/auth";
-import { USER_ROLE } from "../utilis/roles"
+import { USER_ROLE } from "../utilis/roles";
+import { useEffect } from "react";
 
+export function Routes() {
+  const { user, signOut } = useAuth();
 
-export function Routes(){
-    const { user } = useAuth()
+  useEffect(() => {
+    api
+      .get("/users/validated")
+      .catch((error) => {
+        if (
+          error.response?.status === 401
+        )
+          signOut();
+      });
+  }, []);
 
-    function AccessRoutes(){
-        switch(user.role){
-            case USER_ROLE.ADMIN:
-                return <AdminRoutes/>
+  function AccessRoutes() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
 
-            case USER_ROLE.USER:
-                return <UserRoutes/>
-                
-                default: <AuthRoutes/>
-            }
-        }
-            
-    return(
-        <BrowserRouter>
-             { user ? <AccessRoutes/> : <AuthRoutes/>  }
-        </BrowserRouter>
-    )
+      case USER_ROLE.USER:
+        return <UserRoutes />;
+
+      default:
+        <AuthRoutes />;
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      {user ? (
+        <AccessRoutes />
+      ) : (
+        <AuthRoutes />
+      )}
+    </BrowserRouter>
+  );
 }
